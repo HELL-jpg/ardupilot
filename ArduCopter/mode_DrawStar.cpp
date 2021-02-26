fc619e366a4ae1eb6884aadabb672369578eda21
Jerry <littlefrog2333@gmail.com>
2021-02-26 21:48:17 +0800

    五角星已经测试

--- ArduCopter/mode_DrawStar.cpp
+++ ArduCopter/mode_DrawStar.cpp
@@ -10,8 +10,10 @@ bool Copter::ModeDrawStar::init(bool ignore_checks)
     if (copter.position_ok() || ignore_checks) {
         // initialise yaw
         auto_yaw.set_mode_to_default(false);
-        path_num=0;
+
+        path_num = 0;
         generate_path();
+
         // start in position control mode
         pos_control_start();
         return true;
@@ -20,15 +22,25 @@ bool Copter::ModeDrawStar::init(bool ignore_checks)
     }
 }
 
+void Copter::ModeDrawStar::generate_path()
+{
+    float radius_cm = 1000.0;
+
+    wp_nav->get_wp_stopping_point(path[0]);
+
+    path[1] = path[0] + Vector3f(1.0f, 0, 0) * radius_cm;
+    path[2] = path[0] + Vector3f(-cosf(radians(36.0f)), -sinf(radians(36.0f)), 0) * radius_cm;
+    path[3] = path[0] + Vector3f(sinf(radians(18.0f)), cosf(radians(18.0f)), 0) * radius_cm;
+    path[4] = path[0] + Vector3f(sinf(radians(18.0f)), -cosf(radians(18.0f)), 0) * radius_cm;
+    path[5] = path[0] + Vector3f(-cosf(radians(36.0f)), sinf(radians(36.0f)), 0) * radius_cm;
+    path[6] = path[1];
+}
 
 // initialise guided mode's position controller
 void Copter::ModeDrawStar::pos_control_start()
 {
-
     // initialise waypoint and spline controller
     wp_nav->wp_and_spline_init();
-
-
 
     // no need to check return status because terrain data is not used
     wp_nav->set_wp_destination(path[0], false);
@@ -37,39 +49,19 @@ void Copter::ModeDrawStar::pos_control_start()
     auto_yaw.set_mode_to_default(false);
 }
 
-
-
 // guided_run - runs the guided controller
 // should be called at 100hz or more
 void Copter::ModeDrawStar::run()
 {
-    if(path_num<6){
+    if(path_num < 6){
         if(wp_nav->reached_wp_destination()){
-            path_num++;
-            wp_nav->set_wp_destination(path[path_num], false)
+            path_num ++;
+            wp_nav->set_wp_destination(path[path_num], false);
         }
     }
+
     pos_control_run();
-
- }
-
-
-//Éú³ÉÎå½ÇÐÇ
-void Copter::ModeDrawStar::generate_path()
-{
-    float radius_cm=1000;
-    wp_nav->get_wp_stopping_point(path[0]);
-    path[1]=path[0]+Vector3f(1.0f,0,0)*radius_cm;
-    path[2]=path[0]+Vector3f(-cosf(radians(36.0f)),-sinf(radians(36.0f)),0)*radius_cm;
-    path[3]=path[0]+Vector3f(sinf(radians(18.0f)),cosf(radians(18.0f)),0)*radius_cm;
-    path[4]=path[0]+Vector3f(sinf(radians(18.0f)),-cosf(radians(18.0f)),0)*radius_cm;
-    path[5]=path[0]+Vector3f(-cosf(radians(36.0f)),sinf(radians(36.0f)),0)*radius_cm;
-    path[6]=path[1];
-
-
 }
-
-
 
 // guided_pos_control_run - runs the guided position controller
 // called from guided_run
@@ -112,5 +104,3 @@ void Copter::ModeDrawStar::pos_control_run()
         attitude_control->input_euler_angle_roll_pitch_yaw(wp_nav->get_roll(), wp_nav->get_pitch(), auto_yaw.yaw(), true);
     }
 }
-
-
